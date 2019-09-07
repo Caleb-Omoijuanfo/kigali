34ce7b16d48c9c55b9a9218073a1a442560d19ab
caleb Omoijuanfo <c.omoijuanfo@gmail.com>
2019-09-07 08:42:55 +0100

    new course page

--- js/remote_academy.js
+++ js/remote_academy.js
@@ -1,16 +1,4 @@
-var acc=document.getElementsByClassName("accordion");
-var i;
-for (i=0; i<acc.length;i++){
-    acc[i].addEventListener("click",function(){
-        this.classList.toggle("active");
-        var content=this.nextElementSibling;
-            if (content.style.display==="block"){
-                content.style.display="none";
-            }else{
-                content.style.display="block";
-            }
-    })
-}
+
 
 var acc=document.getElementsByClassName("subtopic");
 var i;
